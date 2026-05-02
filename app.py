7c7d203d3330393871a802ddacb574ac2eabeef1
Pranshi34 <prashant14708@gmail.com>
2026-05-02 09:24:05 +0530

    change app.py

--- app.py
+++ app.py
@@ -3,33 +3,35 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 import os
-import tf_keras
 
 app = Flask(__name__)
-model = tf_keras.models.load_model("brain_tumor_model.h5")
+
+# Load model (correct way)
+model = load_model("brain_tumor_model.h5")
 
 class_name = ['glioma', 'meningioma', 'notumor', 'pituitary']
 
 UPLOAD_FOLDER = "static/uploads"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
+# Prediction function
 def predict_image(path):
-    img = load_img(path, target_size=(128,128))
+    img = load_img(path, target_size=(128, 128))
     img = img_to_array(img) / 255.0
     img = np.expand_dims(img, axis=0)
 
     pred = model.predict(img)
     index = np.argmax(pred)
+
     return class_name[index]
 
-
+# Route
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        file = request.files["file"]
+        file = request.files.get("file")
 
-        if file:
+        if file and file.filename != "":
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
             file.save(filepath)
 
@@ -39,6 +41,5 @@ def index():
 
     return render_template("index.html")
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
