dea8031421cac7d7dbb1575633440ead942bb98a
Pranshi34 <prashant14708@gmail.com>
2026-05-02 11:21:22 +0530

    app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from keras.models import load_model
+from tensorflow.modelskeras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 import os
